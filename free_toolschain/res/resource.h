fd3caf8815d712682c765d9e0d9ef1b45ecdd4bb
huangda1982 <huangda1982@1173fb55-4a8d-c345-b236-ac203da12c69>
2010-06-03 14:08:47 +0000

    Compile OK

--- free_toolschain/res/resource.h
+++ free_toolschain/res/resource.h
@@ -1,33 +1,28 @@
-#ifndef	IDC_STATIC
-#define	IDC_STATIC	(-1)
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
 #endif
 
-#define	IDR_MENU			1201
-#define	IDM_HIDE			1202
-#define	IDM_EXIT			1203
-
-#define	IDI_TOPCLOCK	2000
-#define	IDB_0				2100
-#define	IDB_1				2101
-#define	IDB_2				2102
-#define	IDB_3				2103
-#define	IDB_4				2104
-#define	IDB_5				2105
-#define	IDB_6				2106
-#define	IDB_7				2107
-#define	IDB_8				2108
-#define	IDB_9				2109
-#define	IDB_COLON		2110
-#define	IDB_AM			2111
-#define	IDB_PM			2112
-#define	IDB_SEAL			2113
-
-#define	IDD_TOPCLOCK	3000
-#define	IDC_INFO		3001
-#define	IDC_SIGN		3002
-#define	IDC_SEAL		3004
-
-#define	IDC_TOPCLOCK	4000
-#define	IDS_APP_TITLE	4001
-#define	IDS_HIDE	4002
-#define	IDS_EXIT	4003
+#define IDB_0                                   100
+#define IDB_1                                   101
+#define IDB_2                                   102
+#define IDB_3                                   103
+#define IDB_4                                   104
+#define IDB_5                                   105
+#define IDB_6                                   106
+#define IDB_7                                   107
+#define IDB_8                                   108
+#define IDB_9                                   109
+#define IDB_AM                                  110
+#define IDB_PM                                  111
+#define IDB_COLON                               112
+#define IDB_STAMP                               113
+#define IDI_ICON                                200
+#define IDR_MENU                                300
+#define IDM_HIDE                                301
+#define IDM_EXIT                                302
+#define IDD_TOPCLOCK                            400
+#define IDC_VER                                 1000
+#define IDC_INFO                                1002
+#define IDS_HIDE                                40000
+#define IDS_EXIT                                40001
+#define IDS_APP_TITLE                           40002
